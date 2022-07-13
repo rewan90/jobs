87ce619e2e07c389c563c6244497c7e2d981b293
rewan90 <rewan.noufal.1998@yahoo.com>
2022-07-13 16:51:05 +0200

    jobsPages

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
-<signUp/>
+<div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
+    <router-link :to="{ name: 'signUp' }">Sign Up</router-link>
+</div>
 
+<router-view />
 </template>
+
 <script>
 import signUp from './components/signUp.vue'
 export default {
   name: 'App',
-  components: { signUp }
+  components: { }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    max-width: 960px;
+    margin: 0 auto;
 }
 
-body {
-  margin: 0;
-  background: #eee;
+#nav {
+    padding: 30px;
+}
+
+#nav a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+    padding: 10px;
+    border-radius: 4px;
+}
+
+#nav a.router-link-exact-active {
+    color: white;
+    background: crimson;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import signUp from '../components/signUp.vue'
+import Jobs from '../views//jobs/Jobs.vue'
+import JobDetails from '../views//jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: Home
   },
-  
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+{
+    path: '/signup',
+    name: 'signUp',
+    component: signUp
+},
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // 404 catchall
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  }
 ]
 
 const router = createRouter({
