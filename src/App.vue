9d0a156011af8055783e368b07b5ea011689b1c2
rewan90 <rewan.noufal.1998@yahoo.com>
2022-07-13 16:14:24 +0200

    signupPage

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<signUp/>
+
 </template>
+<script>
+import signUp from './components/signUp.vue'
+export default {
+  name: 'App',
+  components: { signUp }
+}
+</script>
 
 <style>
 #app {
@@ -13,18 +17,11 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
